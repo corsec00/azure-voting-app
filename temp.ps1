b6ddb7c392f050a046b2f60f4f041e2a28bfb750
Leo Santos <corsec00@gmail.com>
2024-10-20 13:45:37 -0300

    review 20 oct 24

--- temp.ps1
+++ temp.ps1
@@ -10,16 +10,19 @@ docker ps
 docker-compose down
 
 az login
-$RG = ""
-$myACR = ""
-#$loginServer =""
+$RG = "RG-ResourcesLSS001"
+$myACR = "myacrleoss001"
 az acr create --location "North Central US" --sku standard --admin-enable true --resource-group $RG --name $myACR 
 az acr login --name $myACR
 az acr list --resource-group $RG --query "[].{acrLoginServer:logiServer}" --output table
 docker tag mcr.microsoft.com/azuredocs/azure-vote-front:v1 myacrleoss001.azurecr.io/azure-vote-front:v2
+
 docker images
 docker push myacrleoss001.azurecr.io/azure-vote-front:v2
 az acr repository list --name $myACR --output table
 az acr repository show-tags --name $myACR --repository azure-vote-front --output table
 
 az acr delete -n $myACR --yes
+
+# TS
+## https://learn.microsoft.com/en-us/troubleshoot/azure/azure-kubernetes/connectivity/config-file-is-not-available-when-connecting
